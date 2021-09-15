4764ad5a5e77a5c3a9b5da24901a8ecb8e0732b9
张鹤潇 <zhanghx18@mails.tsinghua.edu.cn>
2021-09-15 18:25:15 +0800

    update code

--- overwrite.py
+++ overwrite.py
@@ -18,15 +18,4 @@ else:
 if chapter == 7:
     os.system("cp overwrite/easy-fs-fuse.rs ../easy-fs-fuse/src/main.rs")
 
-def do_replace(lines: List[str]):
-    for line in lines:
-        line.replace(' git = "https://github.com/rcore-os/riscv"', 'path = "../user/riscv"')
-
-lines = []
-with open("../os/Cargo.toml", 'r') as f:
-    for line in f.readlines():
-        processed = line.replace(' git = "https://github.com/rcore-os/riscv"', ' path = "../ci-user/riscv" ')
-        lines.append(processed)
-        
-with open("../os/Cargo.toml", 'w+') as f:
-    f.writelines(lines)
+os.system("cp overwrite/Cargo.toml ../os/Cargo.toml")
