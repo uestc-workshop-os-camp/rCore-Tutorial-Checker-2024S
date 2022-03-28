aefea20b2a45d8620513a05bd757e477152b0977
Gallium70 <52118815+Gallium70@users.noreply.github.com>
2022-03-28 17:02:28 +0800

    update ch4 checker

--- check/ch4.py
+++ check/ch4.py
@@ -1,22 +1,18 @@
 import base
+from ch3 import EXPECTED, NOT_EXPECTED
 
 
-EXPECTED = [
-    r"get_time OK! (\d+)",
-    "Test sleep OK!",
-    r"current time_msec = (\d+)",
-    r"time_msec = (\d+) after sleeping (\d+) ticks, delta = (\d+)ms!",
-    "Test sleep1 passed!",
+EXPECTED += [
     "Test 04_1 OK!",
     "Test 04_4 test OK!",
     "Test 04_5 ummap OK!",
     "Test 04_6 ummap2 OK!",
 ]
 
-NOT_EXPECTED = [
+NOT_EXPECTED += [
     "Should cause error, Test 04_2 fail!",
     "Should cause error, Test 04_3 fail!",
 ]
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     base.test(EXPECTED, NOT_EXPECTED)
