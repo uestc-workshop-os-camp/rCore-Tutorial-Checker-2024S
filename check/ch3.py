3288100329531b2532646936eb51b26b49520985
TKF <60510632+tkf2019@users.noreply.github.com>
2023-02-08 23:25:05 +0800

    Update ch3.py

--- check/ch3.py
+++ check/ch3.py
@@ -2,11 +2,6 @@ import base
 from ch2 import EXPECTED, NOT_EXPECTED
 
 EXPECTED += [
-    r"get_time OK! (\d+)",
-    "Test sleep OK!",
-    r"current time_msec = (\d+)",
-    r"time_msec = (\d+) after sleeping (\d+) ticks, delta = (\d+)ms!",
-    "Test sleep1 passed!",
     "Test write A OK!",
     "Test write B OK!",
     "Test write C OK!",
